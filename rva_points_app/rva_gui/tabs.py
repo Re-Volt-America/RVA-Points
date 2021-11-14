03d6369e56ad31b1f6ced5ebe8f9e6013b0bb45e
José Benavente <jose@bgmp.cl>
2021-11-14 04:19:30 -0300

    Enlarge result cells

--- rva_points_app/rva_gui/tabs.py
+++ rva_points_app/rva_gui/tabs.py
@@ -283,6 +283,8 @@ class PreviewTab(ScrolledTabPage):
                         self.session_grid.SetColLabelValue(head_col, str(item))
                     else:
                         self.session_grid.SetColLabelValue(head_col, str(item))
+                    if str(item) in ["PP", "PA", "CC", "MP", "PO"]:
+                        self.session_grid.SetColSize(head_col, 80)
                 head_col = head_col + 1
 
     def __fill_preview_content(self):
