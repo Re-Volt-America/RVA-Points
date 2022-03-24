e1469f22315ae1068c04f3895caeeae87f68807c
José Benavente <jose@bgmp.cl>
2022-03-24 09:57:12 -0300

    Implement deployment via GitHub Actions

---
+++ dist/release.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# RVA Points (Generic)
+# Generate json for app info and updates.
+
+import os
+import json
+import hashlib
+import requests
+
+RVGL_URL = "https://distribute.revolt-america.com/rva_points/"
+
+def get_checksum(filename):
+    try:
+        sha = hashlib.sha256()
+        with open(filename, 'rb') as f:
+            while chunk := f.read(256*1024):
+                sha.update(chunk)
+        return sha.hexdigest()
+    except Exception as e:
+        return ""
+
+content = {}
+with open(os.path.join("..", "rva_points_app", "version.py")) as f:
+    exec(f.read(), content)
+
+version = content["__version__"]
+
+appinfo = {}
+appinfo["version"] = version
+
+for platform in ["win64", "linux", "macOS"]:
+    fname = f"rva_points_{platform}.zip"
+    url = f"{RVGL_URL}{platform}/{fname}"
+
+    response = requests.get(url, stream=True)
+    with open(fname, "wb") as f:
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+    appinfo[platform] = {}
+    appinfo[platform]["url"] = url
+    appinfo[platform]["checksum"] = get_checksum(fname)
+
+with open("rva_points.json", "w") as f:
+    json.dump(appinfo, f, indent=4)
---
+++ dist/version.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# RVA Points (Windows)
+# Generate version file for use with PyInstaller.
+
+import os
+from packaging import version
+
+content = {}
+with open(os.path.join("..", "rva_points_app", "version.py")) as f:
+    exec(f.read(), content)
+
+v = version.parse(content["__version__"])
+
+version_text = f"""
+# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+    # Set not needed items to zero 0.
+    filevers={v.release},
+    prodvers={v.release},
+    # Contains a bitmask that specifies the valid bits 'flags'r
+    mask=0x3f,
+    # Contains a bitmask that specifies the Boolean attributes of the file.
+    flags=0x0,
+    # The operating system for which this file was designed.
+    # 0x4 - NT and there is no need to change it.
+    OS=0x40004,
+    # The general type of file.
+    # 0x1 - the file is an application.
+    fileType=0x1,
+    # The function of the file.
+    # 0x0 - the function is not defined for this fileType
+    subtype=0x0,
+    # Creation date and time stamp.
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904e4', [
+        StringStruct(u'FileDescription', u'Re-Volt America Points Calculator'),
+        StringStruct(u'FileVersion', u'{v.public}'),
+        StringStruct(u'InternalName', u'RVA Points'),
+        StringStruct(u'LegalCopyright', u'Copyright (C) BGM 2021-2022'),
+        StringStruct(u'OriginalFilename', u'RVA Points'),
+        StringStruct(u'ProductName', u'RVA Points'),
+        StringStruct(u'ProductVersion', u'{str(v.release)[1:-1]}')])
+      ]),
+    VarFileInfo([VarStruct(u'Translation', [1033, 1252])])
+  ]
+)
+"""
+
+with open("version.txt", "w") as f:
+    f.write(version_text)
